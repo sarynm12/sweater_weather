bd472a91feeeb547742e0a45ea39beeea3f604b0
Saryn Mooney <smooney15@my.whitworth.edu>
2020-11-06 16:21:44 -0700

    add mapquest api call to controller

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,19 +1,26 @@
 class Api::V1::ForecastController < ApplicationController
-  def index
-    conn = Faraday.new(url: "https://api.openweathermap.org")
-    response = conn.get("/data/2.5/weather") do |req|
-      req.params['q'] = 'denver'
+
+  def show
+    conn = Faraday.new('http://www.mapquestapi.com')
+    response = conn.get('/geocoding/v1/address') do |req|
+      req.params['key'] = ENV['MAPQUEST_API_KEY']
+      req.params['location'] = params[:location]
+    end
+    require "pry"; binding.pry
+    coordinates = JSON.parse(response.body, symbolize_names: true)[:results][0][:locations][0][:latLng]
+
+    conn = Faraday.new(url: "https://api.openweathermap.org") do |req|
       req.params['appid'] = ENV['OPEN_WEATHER_API_KEY']
     end
+    response = conn.get("/data/2.5/onecall") do |req|
+      req.params['lat'] = "#{coordinates[:lat]}"
+      req.params['lon'] = "#{coordinates[:lng]}"
+      req.params['exclude'] = 'minutely'
+    end
     data = JSON.parse(response.body, symbolize_names: true)
-    forecast = data[:weather]
+    render json: data
   end
 end
 
-
-# req.params['lat'] = 39.74
-# req.params['lon'] = -104.99
-# req.params['exclude'] = 'minutely'
+# req.params['q'] = params[:location]
 # req.params['appid'] = ENV['OPEN_WEATHER_API_KEY']
-# end
-# response = conn.get('/data/2.5/weather') do |req|
