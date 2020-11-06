d1c29cda287b2a0b8371b544272acadf680c6f3d
Saryn Mooney <smooney15@my.whitworth.edu>
2020-11-06 16:49:07 -0700

    request spec still a work in progress

--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -1,15 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'forecast' do
-
   it 'can retrieve forecast for a given city' do
-
     get '/api/v1/forecast?location=denver,co'
+
     expect(response).to be_successful
     expect(response.content_type).to include("application/json")
+
     json = JSON.parse(response.body, symbolize_names: true)
-    require "pry"; binding.pry
-    expect(json).to have_key(:data)
   end
 
 end
