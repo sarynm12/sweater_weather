38bda7caa28b8c3c12bcf23510c77395ac346f33
Saryn Mooney <smooney15@my.whitworth.edu>
2020-11-07 11:51:36 -0700

    add attribute to forecast serializer

--- app/serializers/forecast_serializer.rb
+++ app/serializers/forecast_serializer.rb
@@ -1,4 +1,6 @@
 class ForecastSerializer
   include FastJsonapi::ObjectSerializer
+  set_type :forecast
+  set_id :'nil?'
   attributes :retrieve_location, :current_forecast, :hourly_forecast, :daily_forecast
 end
