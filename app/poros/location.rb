19efec39f49afe378bfa4c57848ea60f4567b4e9
Saryn Mooney <smooney15@my.whitworth.edu>
2020-11-06 21:02:30 -0700

    create possible location poro

---
+++ app/poros/location.rb
@@ -0,0 +1,16 @@
+# class Location
+#   attr_reader :latitude, :longitude
+#               #:city,
+#
+#               #:state,
+#               #:country,
+#
+#   def initialize(location_attrs)
+#     # @city = attrs[:city]
+#     # @state = attrs[:state]
+#     # @country = attrs[:country]
+#     @latitude = location_attrs[:latLng][:lat]
+#     @longitude = location_attrs[:latLng][:lng]
+#   end
+#
+# end
