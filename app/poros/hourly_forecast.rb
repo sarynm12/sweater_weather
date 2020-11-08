251b5963110b709ed0a25c886a0ef190358b4038
Saryn Mooney <smooney15@my.whitworth.edu>
2020-11-07 20:22:44 -0700

    fix initialize in hourly forecast

--- app/poros/hourly_forecast.rb
+++ app/poros/hourly_forecast.rb
@@ -2,16 +2,16 @@ require 'date'
 class HourlyForecast
   attr_reader :dt,
               :wind_speed,
-              :wind_direction,
-              :conditions,
+              :wind_deg,
+              :description,
               :icon
 
   def initialize(data)
     @dt = Time.at(data[:dt])
     @wind_speed = data[:wind_speed]
-    @wind_direction = data[:wind_direction]
-    @conditions = data[:conditions]
-    @icon = data[:icon]
+    @wind_deg = data[:wind_deg]
+    @description = data[:weather].first[:description]
+    @icon = data[:weather].first[:icon]
   end
 
 end
