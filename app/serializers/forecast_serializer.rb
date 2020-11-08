8578a5f9e001ed3a11298e259f79990b15465486
Saryn Mooney <smooney15@my.whitworth.edu>
2020-11-07 18:31:54 -0700

    add daily weather to serializer

--- app/serializers/forecast_serializer.rb
+++ app/serializers/forecast_serializer.rb
@@ -2,5 +2,5 @@ class ForecastSerializer
   include FastJsonapi::ObjectSerializer
   set_type :forecast
   set_id :'nil?'
-  attributes :retrieve_location, :current_forecast, :hourly_forecast #:daily_forecast
+  attributes :retrieve_location, :current_forecast, :hourly_forecast, :daily_forecast
 end
