14839f473d90a984fef6a11c283c30b3d7d9977a
Saryn Mooney <smooney15@my.whitworth.edu>
2020-11-08 14:10:11 -0700

    update hourly forecast poro to include temp

--- app/poros/hourly_forecast.rb
+++ app/poros/hourly_forecast.rb
@@ -1,6 +1,7 @@
 require 'date'
 class HourlyForecast
   attr_reader :dt,
+              :temp,
               :wind_speed,
               :wind_deg,
               :description,
@@ -8,10 +9,18 @@ class HourlyForecast
 
   def initialize(data)
     @dt = Time.at(data[:dt])
+    @temp = data[:temp]
     @wind_speed = data[:wind_speed]
-    @wind_deg = data[:wind_deg]
+    @wind_deg = cardinal(data[:wind_deg])
     @description = data[:weather].first[:description]
     @icon = data[:weather].first[:icon]
+  end
+
+  def cardinal(deg)
+    val = ((deg/22.5)+0.5)
+    dir = ["N","NNE","NE","ENE","E","ESE", "SE", "SSE","S","SSW","SW","WSW","W","WNW","NW","NNW"]
+    direction = dir[(val % 16)]
+    return direction
   end
 
 end
