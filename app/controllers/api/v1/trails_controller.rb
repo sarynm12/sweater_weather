a0eaffb626a892fe0f4537f724c9831a8f0ff792
Saryn Mooney <smooney15@my.whitworth.edu>
2020-11-09 09:27:55 -0700

    add index action to trails controller, with geocode api call, and trails api call

--- app/controllers/api/v1/trails_controller.rb
+++ app/controllers/api/v1/trails_controller.rb
@@ -1,7 +1,22 @@
 class Api::V1::TrailsController < ApplicationController
 
   def index
-
-  end 
+    #get coordinates from mapquest api
+    conn = Faraday.new('http://www.mapquestapi.com')
+    response = conn.get('/geocoding/v1/address') do |req|
+      req.params['key'] = ENV['MAPQUEST_API_KEY']
+      req.params['location'] = params[:location]
+    end
+    coordinates = JSON.parse(response.body, symbolize_names: true)[:results][0][:locations][0][:latLng]
+    #pass coordinates into trails request
+    conn = Faraday.new(url: 'https://www.hikingproject.com')
+    response = conn.get('/data/get-trails') do |req|
+      req.params['key'] = ENV['TRAILS_API_KEY']
+      req.params['lat'] = "#{coordinates[:lat]}"
+      req.params['lon'] = "#{coordinates[:lng]}"
+    end
+    data = JSON.parse(response.body, symbolize_names: true)
+    render json: data
+  end
 
 end
