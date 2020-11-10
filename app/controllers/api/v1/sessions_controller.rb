e42321225c267ba35e93a77256e2c72691e8ffc5
Saryn Mooney <smooney15@my.whitworth.edu>
2020-11-10 09:14:00 -0700

    add json error message

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,9 +3,9 @@ class Api::V1::SessionsController < ApplicationController
   def create
     user = User.find_by(email: session_params[:email])
     if user && user.authenticate(params[:password])
-      render json: UserSerializer.new(user), status: 200 }
+      render json: UserSerializer.new(user), status: 200
     else
-      render json: { 'error': 'Credentials are Bad'}, status: 400
+      render json: { 'error': 'Credentials are Bad' }, status: 400
     end
   end
 
