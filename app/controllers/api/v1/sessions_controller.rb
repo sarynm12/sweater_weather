0c5e8f5b7c643b07940a4107e43481caf6350708
Saryn Mooney <smooney15@my.whitworth.edu>
2020-11-09 20:50:49 -0700

    add user serializer to sessions controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::SessionsController < ApplicationController
   def create
     user = User.find_by(email: session_params[:email])
     if user && user.authenticate(params[:password])
-      render json: { user, {'message': 'Success'} status: 200 }
+      render json: UserSerializer.new(user), status: 200 }
     else
       render json: { 'error': 'Credentials are Bad'}, status: 400
     end
